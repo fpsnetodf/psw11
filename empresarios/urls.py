0965ffe5540a858bbc680ae29f676855360ab428
fps_netodf <fps_netodf@hotmail.com>
2024-08-06 16:55:03 -0300

    add choices

--- empresarios/urls.py
+++ empresarios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("empresarios/", views.empresarios, name="empresarios")
+    path("empresarios/", views.empresarios, name="empresarios"),
+    path("cadastrar_empresa/", views.cadastrar_empresa, name="cadastrar-empresa")
 ]
--- empresarios/views.py
+++ empresarios/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from .models import Empresas
 # Create your views here.
 
 
 def empresarios(request):
     return render(request, "empresarios/empresarios.html")
+
+
+def cadastrar_empresa(request):
+    if request.method == "GET":
+        tempos = Empresas.tempo_existencia_choices
+        areas = Empresas.area_choices
+        return render(request, "empresarios/cadastrar_empresa.html", {"tempos": tempos, "areas": areas })
